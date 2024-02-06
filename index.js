6c2637bc153b7a4ef6ac863b920956fb8b6c0611
Napzeira <112515227+Napzeira@users.noreply.github.com>
2024-02-06 10:12:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Client } = require('discord.js-selfbot-v11')
 const client = new Client();
 
 client.on('ready', () => { 
-  client.user.setActivity("Put Activity here", {type: "WATCHING/LISTENING/STREAMING/PLAYING", url: "https://twitch.tv/ninja"}) // put a twitch url if you selected STREAMING as your activity
+  client.user.setActivity("Counter-Strike 2", {type: "PLAYING", url: ""}) // put a twitch url if you selected STREAMING as your activity
    console.log(`Log in account name: ${client.user.username}`)
 })
 
